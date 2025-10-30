d3ae9bae869952ebde4566c0bb91872a651c476c
Pranav Kolambkar <pranavkolambkar23@gmail.com>
2025-10-30 21:40:54 +0530

    update url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 // src/api/client.ts
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // const API_BASE_URL = "http://127.0.0.1:8000/api";
 
